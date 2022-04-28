3c7f336b757c4a212aa35639bfb1252ee161db25
AlshamiWasin <waseem.fr1999@gmail.com>
2022-04-28 12:54:39 +0200

    j'ai ajouter test.js

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ panier = [{produit:produit, quantite: 1}];
 
 
 
-function ajouteraupanier(produit , quantite){
+export function ajouteraupanier(produit , quantite){
     if (produit && quantite != 0) {
         panier.push({produit:produit,quantite:quantite})
     }
 }
 
-
-
-function calculerTVA(){
+export function calculerTVA(){
     let tva = 0;
     panier.forEach(element => {
         tva += (20/100*element.produit.prix) * element.quantite;
@@ -19,7 +17,7 @@ function calculerTVA(){
     return tva
 }
 
-function calculerPrixTotal(){
+export function calculerPrixTotal(){
     let PrixTotal = 0;
     panier.forEach(element => {
         PrixTotal += ((element.produit.prix) * element.quantite);
@@ -28,7 +26,7 @@ function calculerPrixTotal(){
     return PrixTotal
 }
 
-ajouteraupanier(produit,2)
-console.log(panier);
-console.log(calculerTVA());
-console.log(calculerPrixTotal());
+// ajouteraupanier(produit,2)
+// console.log(panier);
+// console.log(calculerTVA());
+// console.log(calculerPrixTotal());
---
+++ tests/test.js
@@ -0,0 +1,13 @@
+import should from "should"
+import {ajouteraupanier} from "./index.js"
+import {calculerTVA} from "./index.js"
+import {calculerPrixTotal} from "./index.js"
+
+
+describe("Checking if the panier is created correctly", function(){  
+    it("should create the user with the correct name", function(){
+        debugger
+        console.log(calculerTVA());
+        console.log(calculerPrixTotal());
+    });
+});
